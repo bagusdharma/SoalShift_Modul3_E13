8c1a921b914d2d8bcec22bbeb5abb0b4ff0c7eee
bagusdharma <bagus.dharma@gmail.com>
2017-10-25 00:42:43 +0700

    add a input user

--- soal4.c
+++ soal4.c
@@ -5,14 +5,15 @@
 
 void *faktorial( void *ptr );
 
-int main()
+int main(int argc, char *argv[])
 {
 	pthread_t tid[2];
 	int i = 0;
 	int err;
-	int input[2]={3,2};
+	int input[2];
 	for (i=0;i<2;i++)
 	{
+	   input[i] = argv[i+1][0]-'0';
 	   err=pthread_create(&(tid[i]),NULL,&faktorial,&(input[i]));//membuat thread
 	    if(err!=0) {
 	        printf("\n can't create thread : [%s]",strerror(err));
