b8262b73066cb6f3e948c11e921ad707843d486e
bagusdharma <bagus.dharma@gmail.com>
2017-10-25 00:34:03 +0700

    try a new thread code

--- soal4.c
+++ soal4.c
@@ -7,43 +7,35 @@ void *faktorial( void *ptr );
 
 int main()
 {
-     pthread_t thread1, thread2;//inisialisasi awal
-     const char *message1 = "Thread 1";
-    // const char *message2 = "Thread 2";
-     int  iret1, iret2;
-
-     iret1 = pthread_create( &thread1, NULL, faktorial, (void*) message1);//membuat thread pertama
-     if(iret1)//jika eror
-     {
-         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret1);
-         exit(EXIT_FAILURE);
-     }
-
-    /* iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);//membuat thread kedua
-     if(iret2)//jika gagal
-     {
-         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
-         exit(EXIT_FAILURE);
-     }*/
-
-	printf("pthread_create() for thread 1 returns: %d\n",iret1);
-    // printf("pthread_create() for thread 2 returns: %d\n",iret2);
-
-     //pthread_join( thread1, NULL);
-     //pthread_join( thread2, NULL); 
-
-     exit(EXIT_SUCCESS);
+	pthread_t tid[2];
+	int i = 0;
+	int err;
+	int input[2]={3,2};
+	for (i=0;i<2;i++)
+	{
+	   err=pthread_create(&(tid[i]),NULL,&faktorial,&(input[i]));//membuat thread
+	    if(err!=0) {
+	        printf("\n can't create thread : [%s]",strerror(err));
+            }
+	    else {
+	        //printf("\n create thread success\n");
+            }
+	    pthread_join(tid[i],NULL);
+	}
 }
 
 void *faktorial( void *ptr )
 {
-     int num1,num2,num3,result,i;
+     int i,result;
      result=i=1;
-     printf("Faktorial "); scanf ("%d", &num1);
+     int *x = (int *) ptr;
+	printf ("%d",*x);
 
-	while (i<=num1) {
+	while (i<=(*x)) {
 		result*=i;
 		i++;
 	}
-	printf ("hasil %d ! = %d",num1,result);
+	return NULL;
+	//printf ("hasil %d ! = %d",num1,result);
 }
+
