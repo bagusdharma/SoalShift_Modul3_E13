ba6670602b1340811057591b7cb2b2c0d2b2d7c8
bayusatriat <bayusatria17@gmail.com>
2017-10-25 08:48:29 +0700

    fix kepiting

--- soal3.c
+++ soal3.c
@@ -6,11 +6,13 @@
 #include<stdlib.h>
 
 int flag=0;
-int lohan=100,kepiting=100;
+int lohan=100;
+int kepiting=100;
 pthread_t tid[2];
 
 void* hewan(void *arg)
 {
+    pthread_t id=pthread_self();
     if (pthread_equal(id,tid[0]))
     {
 	while (lohan>0&&lohan<=100)
@@ -27,7 +29,7 @@ void* hewan(void *arg)
     {
 	while (kepiting>0&&kepiting<=100)
 	{
-	    printf("Status kepiting: %d\n");
+	    printf("Status kepiting: %d\n",kepiting);
 	    sleep(20);
 	    kepiting-=10;
 	}
@@ -35,4 +37,26 @@ void* hewan(void *arg)
 	flag=1;
     }
 
+}
+
+int main(void)
+{
+    int i=0;
+    int err;
+    while(i<2)//looping membuat thread 2x
+    {
+        err=pthread_create(&(tid[i]),NULL,&hewan,NULL);//membuat thread
+        if(err!=0)//cek error
+        {
+            printf("\n can't create thread : [%s]",strerror(err));
+        }
+        else
+        {
+            printf("\n create thread success");
+        }
+        i++;
+    }
+    pthread_join(tid[0],NULL);
+    pthread_join(tid[1],NULL);
+    return 0;
 }
