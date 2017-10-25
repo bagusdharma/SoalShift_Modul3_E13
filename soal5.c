414031e848ce5b00f6501503917e48ed088fd0fc
bagusdharma <bagus.dharma@gmail.com>
2017-10-25 12:49:33 +0700

    fix the code + add find input function

--- soal5.c
+++ soal5.c
@@ -8,6 +8,7 @@ struct tes {
 	int len,result;
 };
 
+void *cari (void *a);
 void baca();
 
 char str[5000];
@@ -22,6 +23,14 @@ int main(int argc,char *argv[]) {
 		while(i+1 < argc) {
 	    		strcpy((tes1[i]).input,argv[i+1]);
 			tes1[i].len=strlen(argv[i+1]);
+			err=pthread_create(&(tid[i]),NULL,&cari,&(tes1[i]));//membuat thread
+			if(err!=0) {
+				printf("\n can't create thread : [%s]",strerror(err));
+		        }
+		        else {
+			        //printf("\n create thread success\n");
+	                }
+		        pthread_join(tid[i],NULL);
 			i++;
 		}
 		for(i=0;i<argc-1;i++) {
@@ -47,3 +56,22 @@ void baca()
 }
 
 
+void *cari (void *a) {
+	int i,result,j=0,count=0;
+	result=i=0;
+	struct tes *tes1 = (struct tes *)a;
+	for (i=0;i<=length;i++) {
+		if(str[i] == ((*tes1).input)[j]) {
+			if(j == (*tes1).len-1) {
+				count++;
+				j=0;
+			}
+			else {
+				j++;
+			}
+		}
+	}
+	(*tes1).result=count;
+
+	return NULL;
+}
