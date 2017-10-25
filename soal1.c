610d4a71531c748d315fade949929cd41591e8a9
bayusatriat <bayusatria17@gmail.com>
2017-10-25 14:33:52 +0700

    tambahan fungsi pembeli

--- soal1.c
+++ soal1.c
@@ -13,24 +13,24 @@ void main()
 
     scanf("%d",&siapa);
 
+    key_t key1=1111,key2=2222,key3=3333,key4=4444,key5=5555,key6=6666;
+    int *a1,*a2,*a3,*a4,*a5,*a6;
 
-	key_t key1=1111,key2=2222,key3=3333,key4=4444,key5=5555,key6=6666;
-	int *a1,*a2,*a3,*a4,*a5,*a6;
+    int shmid1 = shmget(key1, sizeof(int), IPC_CREAT | 0666);
+    a1 = shmat(shmid1, NULL, 0);
+    int shmid2 = shmget(key2, sizeof(int), IPC_CREAT | 0666);
+    a2 = shmat(shmid2, NULL, 0);
+    int shmid3 = shmget(key3, sizeof(int), IPC_CREAT | 0666);
+    a3 = shmat(shmid3, NULL, 0);
+    int shmid4 = shmget(key4, sizeof(int), IPC_CREAT | 0666);
+    a4 = shmat(shmid4, NULL, 0);
+    int shmid5 = shmget(key5, sizeof(int), IPC_CREAT | 0666);
+    a5 = shmat(shmid5, NULL, 0);
+    int shmid6 = shmget(key6, sizeof(int), IPC_CREAT | 0666);
+    a6 = shmat(shmid6, NULL, 0);
 
-	int shmid1 = shmget(key1, sizeof(int), IPC_CREAT | 0666);
-	a1 = shmat(shmid1, NULL, 0);
-        int shmid2 = shmget(key2, sizeof(int), IPC_CREAT | 0666);
-        a2 = shmat(shmid2, NULL, 0);
-        int shmid3 = shmget(key3, sizeof(int), IPC_CREAT | 0666);
-        a3 = shmat(shmid3, NULL, 0);
-        int shmid4 = shmget(key4, sizeof(int), IPC_CREAT | 0666);
-        a4 = shmat(shmid4, NULL, 0);
-        int shmid5 = shmget(key5, sizeof(int), IPC_CREAT | 0666);
-        a5 = shmat(shmid5, NULL, 0);
-        int shmid6 = shmget(key6, sizeof(int), IPC_CREAT | 0666);
-        a6 = shmat(shmid6, NULL, 0);
+    *a1=0; *a2=0; *a3=0; *a4=0; *a5=0; *a6=0;
 
-	*a1=0; *a2=0; *a3=0; *a4=0; *a5=0; *a6=0;
     if (siapa==1)
     {
 	while (1)
@@ -68,16 +68,55 @@ void main()
 	    }
 	}
     }
-	shmdt(a1);
-	shmctl(shmid1, IPC_RMID, NULL);
-	shmdt(a2);
-        shmctl(shmid2, IPC_RMID, NULL);
-	shmdt(a3);
-        shmctl(shmid3, IPC_RMID, NULL);
-	shmdt(a4);
-        shmctl(shmid4, IPC_RMID, NULL);
-	shmdt(a5);
-        shmctl(shmid5, IPC_RMID, NULL);
-	shmdt(a6);
-        shmctl(shmid6, IPC_RMID, NULL);
+
+    if (siapa==2)
+    {
+	while (1)
+	{
+	    int buatpembeli=0;
+	    printf("Ketik 1 = lihat stock\n");
+	    printf("Ketik 2 = beli senjata\n");
+
+	    scanf("%d",&buatpembeli);
+
+	    if (buatpembeli==1)
+	    {
+		printf("MP4A1 = %d buah\n",*a1);
+		printf("PM2-V1 = %d buah\n",*a2);
+		printf("SPR-3 = %d buah\n",*a3);
+		printf("SS2-V5 = %d buah\n",*a4);
+		printf("SPG1-V3 = %d buah\n",*a5);
+		printf("MINE = %d buah\n",*a6);
+	    }
+
+	    if (buatpembeli==2)
+	    {
+		char namanya[10];
+		int jumlahnya;
+		printf("Masukkan senjata yang ingin dibeli: ");
+		scanf("%s",namanya);
+		scanf("%d",&jumlahnya);
+		if (strcmp(namanya,"MP4A1")==0&&*a1>=jumlahnya) *a1-=jumlahnya;
+		else if (strcmp(namanya,"PM2-V1")==0&&*a2>=jumlahnya) *a2-=jumlahnya;
+		else if (strcmp(namanya,"SPR-3")==0&&*a3>=jumlahnya) *a3-=jumlahnya;
+		else if (strcmp(namanya,"SS2-V5")==0&&*a4>=jumlahnya) *a4-=jumlahnya;
+		else if (strcmp(namanya,"SPG1-V3")==0&&*a5>=jumlahnya) *a5-=jumlahnya;
+		else if (strcmp(namanya,"MINE")==0&&*a6>=jumlahnya) *a6-=jumlahnya;
+		else printf("Senjata tidak tersedia atau stok tidak cukup\n");
+	    }
+	}
+    }
+
+    shmdt(a1);
+    shmctl(shmid1, IPC_RMID, NULL);
+    shmdt(a2);
+    shmctl(shmid2, IPC_RMID, NULL);
+    shmdt(a3);
+    shmctl(shmid3, IPC_RMID, NULL);
+    shmdt(a4);
+    shmctl(shmid4, IPC_RMID, NULL);
+    shmdt(a5);
+    shmctl(shmid5, IPC_RMID, NULL);
+    shmdt(a6);
+    shmctl(shmid6, IPC_RMID, NULL);
 }
